94490773ccacea850e22fd0c6e8caed0d51c73a7
NickYang <elite_yang@163.com>
2016-09-12 20:56:00 +0800

    new functions

--- ArkNet/Connection.cpp
+++ ArkNet/Connection.cpp
@@ -18,43 +18,154 @@ namespace ArkNet
 		Stop();
 	}
 
-	void Connection::Start()
-	{
-
-	}
-
 	void Connection::Stop()
 	{
-		//TODO:disconnect event
+		//TODO:server disconnected event
 		
 		mbConnected = false;
 		boost::system::error_code ec;
 		mxSocket.close(ec);
 	}
 
+    void Connection::Close()
+    {
+        mxConnectionMgr->Stop(shared_from_this());
+    }
+
+    void Connection::Start()
+    {
+        mxResponseBuffer.consume(mxResponseBuffer.size());
+        mbConnected = true;
+
+        boost::system::error_code error;
+        mxSocket.set_option(boost::asio::ip::tcp::no_delay(true), error);
+        if (error)
+        {
+            std::cout << "set tcp::no_delay failed, error info : " << error.message();
+        }
+
+        //TODO:server connected event
+
+        //TODO:4是包头长度
+        boost::asio::async_read(mxSocket, mxResponseBuffer, boost::asio::transfer_exactly(4),
+            std::bind(&Connection::HandleReadHeader,
+                shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred)
+        );
+
+    }
+
+    void Connection::HandleReadHeader(const boost::system::error_code& error, std::size_t nTransferedBytes)
+    {
+        if (error || !mbConnected)
+        {
+            Close();
+            return;
+        }
+
+        boost::asio::streambuf tempbuf;
+        boost::asio::buffer_copy(tempbuf.prepare(mxResponseBuffer.size()), mxResponseBuffer.data());
+        tempbuf.commit(mxResponseBuffer.size());
+
+        int32_t nPacketLength = 0;
+        tempbuf.sgetn((char*)(&nPacketLength), sizeof(nPacketLength));
+        nPacketLength = ntohl(nPacketLength);
+
+        if (nPacketLength > 512 * 1024)
+        {
+            Close();
+            return;
+        }
+
+        boost::asio::async_read(mxSocket, mxResponseBuffer, boost::asio::transfer_at_least(nPacketLength), 
+            std::bind(&Connection::HandleReadBody, 
+                shared_from_this(), 
+                boost::asio::placeholders::error, 
+                boost::asio::placeholders::bytes_transferred)
+        );
+    }
+
+    void Connection::HandleReadBody(const boost::system::error_code& error, std::size_t nTransferedBytes)
+    {
+        if (error || !mbConnected)
+        {
+            Close();
+            return;
+        }
+
+        int nPacketLength = static_cast<int>(nTransferedBytes + 4); //???
+        std::string strMsg;
+        strMsg.resize(nPacketLength);
+        mxResponseBuffer.sgetn(&strMsg[0], nPacketLength);
+        mxResponseBuffer.consume(nPacketLength);
+
+        mxServer.ProcessMsg(msg, shared_from_this());
+
+        boost::asio::async_read(mxSocket, mxResponseBuffer, 
+            std::bind(&Connection::HandleReadHeader, 
+                shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred)
+        );
+    }
+
 	void Connection::WriteMsg(const std::string& msg, const WriteCallback& cb /*= WriteCallback()*/)
 	{
-
-	}
-
-	void Connection::Close()
-	{
-		mxConnectionMgr->Stop(shared_from_this());
-	}
-
-	void Connection::HandleRead(const boost::system::error_code& error, std::size_t nTransferedBytes)
-	{
-
+        mxIOS.post(std::bind(&Connection::DoWrite, shared_from_this(), msg, cb));
 	}
 
 	void Connection::HandleWrite(const boost::system::error_code& error)
 	{
+        WriteCallback& cb = mxWriteQueue.front().handler;
+        if (cb)
+        {
+            cb(error);
+        }
 
+        if (error)
+        {
+            Close();
+        }
+        else
+        {
+            mxWriteQueue.pop_front();
+            if (!mxWriteQueue.empty())
+            {
+                boost::asio::async_write(mxSocket,
+                    boost::asio::buffer(mxWriteQueue.front().msg.data(), mxWriteQueue.front().msg.length()),
+                    std::bind(&Connection::HandleWrite,
+                        shared_from_this(),
+                        boost::asio::placeholders::error)
+                );
+            }
+        }
 	}
 
-	void Connection::DoWrite(std::string msg, WriteCallback cb)
-	{
+    void Connection::DoWrite(std::string msg, WriteCallback cb)
+    {
+        bool bWriting = !mxWriteQueue.empty();
+        InternelMsg item = { msg, cb };
+        mxWriteQueue.push_back(item);
+        if (!bWriting)
+        {
+            boost::asio::async_write(mxSocket, 
+                boost::asio::buffer(mxWriteQueue.front().msg.data(), mxWriteQueue.front().msg.length()), 
+                std::bind(&Connection::HandleWrite, 
+                    shared_from_this(), 
+                    boost::asio::placeholders::error)
+            );
+        }
+    }
 
-	}
+    boost::any Connection::GetProperty(const std::string& property)
+    {
+        return mxConnectPropertyMap[property];
+    }
+
+    void Connection::SetProperty(const std::string& property, const boost::any& value)
+    {
+        mxConnectPropertyMap[property] = value;
+    }
 
 }
--- ArkNet/Connection.h
+++ ArkNet/Connection.h
@@ -1,7 +1,14 @@
 #pragma once
+
+#include <deque>
+#include <set>
+#include <algorithm>
 #include "boost/noncopyable.hpp"
 #include "boost/enable_shared_from_this.hpp"
 #include "boost/asio.hpp"
+#include "boost/any.hpp"
+#include "boost/thread.hpp"
+
 
 namespace ArkNet
 {
@@ -23,9 +30,12 @@ namespace ArkNet
 		typedef std::function<void(const boost::system::error_code&)> WriteCallback;
 		void WriteMsg(const std::string& msg, const WriteCallback& cb = WriteCallback());
 
+        boost::any GetProperty(const std::string& property);
+        void SetProperty(const std::string& property, const boost::any& value);
 	protected:
 		void Close();
-		void HandleRead(const boost::system::error_code& error, std::size_t nTransferedBytes);
+		void HandleReadHeader(const boost::system::error_code& error, std::size_t nTransferedBytes);
+        void HandleReadBody(const boost::system::error_code& error, std::size_t nTransferedBytes);
 		void HandleWrite(const boost::system::error_code& error);
 
 		void DoWrite(std::string msg, WriteCallback cb);
@@ -36,6 +46,54 @@ namespace ArkNet
 		boost::asio::ip::tcp::socket mxSocket;
 		ConnectionManager* mxConnectionMgr;
 		bool mbConnected;
+        boost::asio::streambuf mxRequestBuffer;
+        boost::asio::streambuf mxResponseBuffer;
+
+        struct InternelMsg 
+        {
+            std::string msg;
+            WriteCallback handler;
+        };
+
+        typedef std::deque<InternelMsg> WriteQueue;
+        WriteQueue mxWriteQueue;
+        std::map<std::string, boost::any> mxConnectPropertyMap;
 	};
 
+
+    typedef std::shared_ptr<Connection> ConnectionPtr;
+    typedef std::weak_ptr<Connection> ConnenctionWeakPtr;
+
+    class ConnectionManager : public boost::noncopyable 
+    {
+    public:
+        void Start(ConnectionPtr connect)
+        {
+            boost::mutex::scoped_lock lock(mxMutex);
+            mxConnections.insert(connect);
+            connect->Start();
+        }
+
+        void Stop(ConnectionPtr connect)
+        {
+            boost::mutex::scoped_lock lock(mxMutex);
+            if (mxConnections.find(connect) != mxConnections.end())
+            {
+                mxConnections.erase(connect);
+            }
+
+            connect->Stop();
+        }
+
+        void StopAll()
+        {
+            boost::mutex::scoped_lock lock(mxMutex);
+            std::for_each(mxConnections.begin(), mxConnections.end(), std::bind(&Connection::Stop, std::placeholders::_1));
+            mxConnections.clear();
+        }
+    protected:
+    private:
+        boost::mutex mxMutex;
+        std::set<ConnectionPtr> mxConnections;
+    };
 }
